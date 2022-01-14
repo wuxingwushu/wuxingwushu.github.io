b348428ae1bdfd58ee8fb3a975fc15cd39ae2db8
吾行误述 <49816732+wuxingwushu@users.noreply.github.com>
2022-01-15 01:41:24 +0800

    Update run.js

--- run.js
+++ run.js
@@ -1,5 +1,8 @@
 var bool = 1;
 var ulid = "";
+var leixing = ["p","h1","h2","a"];
+var daimaleixing = ["var","#569CD6","case","#569CD6","break","#569CD6","for","#c586c0","switch","#c586c0","function","#569CD6","style","#4fc1ff","if","#c586c0","else","#c586c0"];
+var lieshu = 0;
 
 function donghuakai(IDshu){
 
@@ -42,7 +45,7 @@ function donghuakai(IDshu){
     if(pos >= -kuan*0.045 && pos1 <= kuan*0.255 && pos2 <= -kuan*0.645)
     {
       elem.style.left = -kuan*0.045 + "px";
-      elem1.style.left = kuan*0.255 + "px";
+      elem1.style.left = kuan*0.254 + "px";
       elem2.style.left = -kuan*0.645 + "px";
       clearInterval(id);
     }
@@ -100,40 +103,47 @@ function donghuatuen(IDshu){
 
 
 
-
+//删除主键
+function shanchu(neirong)
+{
+  var child=document.getElementById(neirong);
+  child.parentNode.removeChild(child);
+}
 
 //添加主键
 function addli(type,TXT,shu,TXT1,shu1)
 {
   var ele = document.createElement(type);//创建一个LI
-  ele.innerHTML = TXT;//修改里面的属性
-  ele.id = TXT;
+  ele.innerHTML = TXT;//修改里面的属性s
+  //ele.id = TXT;
   ele.onmouseover = function() {yulan(TXT1,shu1);}
+
+  var ul = ""
+  switch(shu){
+    case "3":ul ="Game/" + TXT + ".txt";break;
+    case "7":ul ="Dentury/" + TXT + ".txt";break;
+  }
+
+  ele.onclick = function() {duqutxtneirong(ul);}
   document.getElementById(shu).appendChild(ele);//把LI放到ID="zhu"的lu里面
 }
-
+//添加空建
+function addlifu(type,shu)
+{
+  var ele = document.createElement(type);//创建一个LI
+  document.getElementById(shu).appendChild(ele);//把LI放到ID="zhu"的lu里面
+}
+//显示简介
 function yulan(TXT,id)
 {
   var child=document.getElementById(id);
   child.innerHTML=TXT;
 }
 
-function addlifu(type,shu)
-{
-  var ele = document.createElement(type);//创建一个LI
-  document.getElementById(shu).appendChild(ele);//把LI放到ID="zhu"的lu里面
-}
 
 
 
 
-
-//删除主键
-function shanchu(neirong)
-{
-  var child=document.getElementById(neirong);
-child.parentNode.removeChild(child);
-}
 
 /*
 function readFiles(){
@@ -160,11 +170,13 @@ function urlToBlob(URss,id,zhujian,id1) {
       reader.onload = function () {
         //console.log('reader.result', reader.result)
         
-
+        for (let index = 0; index < 4; index++){
+          addlifu("li",id);
+        }
         for (let index = 0; index < reader.result.split("\n").length-1; index=index+2) {
           addli(zhujian,reader.result.split("\n")[index],id,reader.result.split("\n")[index+1],id1);
         }
-        for (let index1 = 0; index1 < 10; index1++) {
+        for (let index1 = 0; index1 < 5; index1++) {
           addlifu("li",id);
         }
 
@@ -176,19 +188,6 @@ function urlToBlob(URss,id,zhujian,id1) {
 }
 
 
-function chushihua(TXT,id,id1){ 
-  for (let index = 0; index < 4; index++){
-    addlifu("li",id);
-  }
-  urlToBlob(TXT,id,"li",id1);
-}
-
-
-chushihua('Game/游戏日志.txt',"3","xtxt0");
-chushihua('Dentury/事记.txt',"7","xtxt1");
-//alert("如果无法正常链接Github的数据！\n可以使用Steam++后再次尝试！");
-
-
 
 
 
@@ -198,27 +197,24 @@ var scrollFunction = function(e) {
   e.preventDefault && e.preventDefault(); //禁止浏览器默认事件
   if (e.wheelDelta) { //判断浏览器IE，谷歌滑轮事件
       if (e.wheelDelta > 0) { //当滑轮向上滚动时
-        ulyidong(100);
+        ulyidong(-100);
       }
       if (e.wheelDelta < 0) { //当滑轮向下滚动时
-        ulyidong(-100);
+        ulyidong(100);
       }
 
   } else if (e.detail) { //Firefox滑轮事件
       if (e.detail > 0) { //当滑轮向上滚动时
-        ulyidong(100);
+        ulyidong(-100);
       }
       if (e.detail < 0) { //当滑轮向下滚动时
-        ulyidong(-100);
+        ulyidong(100);
       }
   }
 }
-
 function ulyidong(shu){
   document.getElementById(ulid).scrollTop=document.getElementById(ulid).scrollTop+shu;
 }
-
-
 //禁止浏览器默认事件
 function shubiaogundong(){
     //给页面绑定滑轮滚动事件
@@ -228,8 +224,6 @@ function shubiaogundong(){
   //滚动滑轮触发scrollFunction方法  //ie 谷歌
   window.addEventListener('mousewheel', scrollFunction, {passive: false});
 }
-
-
 //开启浏览器默认事件
 function shubiaogundong0(){
   //给页面解绑滑轮滚动事件
@@ -238,4 +232,217 @@ function shubiaogundong0(){
   }
   //滚动滑轮触发scrollFunction方法  //ie 谷歌
   window.removeEventListener('mousewheel', scrollFunction);
+}
+
+
+
+
+
+
+
+urlToBlob('Game/游戏日志.txt',"3","li","xtxt3");
+urlToBlob('Dentury/事记.txt',"7","li","xtxt7");
+//alert("如果无法正常链接Github的数据！\n可以使用Steam++后再次尝试！");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function zhifuchuan(shei,shu,TXT){
+  return shei = shei.substr(0,shu) + TXT + shei.substr(shu,10000);
+}
+
+
+
+
+
+
+
+//添加代码主键
+function addma(type,TXT,shu)
+{
+  var divs = "div" + lieshu;
+  var divt = document.createElement("div");//创建一个LI
+  divt.id = divs;
+  divt.style.height = "auto";
+  divt.style.width = "90%";
+  divt.style.left = "5%";
+  divt.style.position = "relative";
+  divt.style.background = "#303030"
+  //divt.style.transform = "skew(12deg)";
+  document.getElementById(shu).appendChild(divt);
+
+
+
+  var txtdata = [];
+  for (let index = 0; index < daimaleixing.length; index=index+2) {
+    var i = 1;
+    var to = 0;
+    while(i){
+      if(TXT.substr(to,10000).search(daimaleixing[index]) != -1){
+        to = to + TXT.substr(to,10000).search(daimaleixing[index]);
+        txtdata.push(to);
+        txtdata.push(daimaleixing[index+1]);
+        to = to + daimaleixing[index].length;
+        txtdata.push(to);
+      }else{
+        i = 0;
+      }
+    }
+  }
+  var len = txtdata.length;
+  var tmp1 = null;
+  var tmp2 = null;
+  var tmp3 = null;
+  for(var j=0; j<len-3; j=j+3){
+    for(var i=0; i<len-3-j; i=i+3){
+    if(txtdata[i]>txtdata[i+3]){
+        tmp1 = txtdata[i];
+        tmp2 = txtdata[i+1];
+        tmp3 = txtdata[i+2];
+
+
+        txtdata[i] = txtdata[i+3];
+        txtdata[i+1] = txtdata[i+4];
+        txtdata[i+2] = txtdata[i+5];
+
+
+        txtdata[i+3] = tmp1;
+        txtdata[i+4] = tmp2;
+        txtdata[i+5] = tmp3;
+    }
+    }
+  }
+  console.log(txtdata);
+  var xinTXT = TXT.substr(0,txtdata[0]);
+  xinTXT = xinTXT + "<span style='color:" + txtdata[1] + "'>" + TXT.substr(txtdata[0],(txtdata[2]-txtdata[0])) + "</span>";
+  for (let index = 3; index < txtdata.length; index=index+3) {
+    xinTXT = xinTXT + TXT.substr(txtdata[index-1],(txtdata[index]-txtdata[index-1]));
+    xinTXT = xinTXT + "<span style='color:" + txtdata[index+1] + "'>" + TXT.substr(txtdata[index],(txtdata[index+2]-txtdata[index])) + "</span>";
+  }
+  xinTXT = xinTXT + TXT.substr(txtdata[txtdata.length-1],10000);
+
+  if(xinTXT.search("//") != -1){
+    xinTXT = xinTXT.substr(0,xinTXT.search("//")) + "<span style='color:#6a9955'>" + xinTXT.substr(xinTXT.search("//"),10000) + "</span>";
+  }
+
+
+  console.log(xinTXT);
+  var ele = document.createElement("p");//创建一个LI
+  ele.style ='white-space:pre;';
+  ele.innerHTML = xinTXT;//修改里面的属性s
+  //ele.style.width = "100%";
+  ele.style.color = "#ffffff";
+  ele.className = type;
+  //ele.style.float = "left";
+  document.getElementById(divs).appendChild(ele);//把LI放到ID="zhu"的lu里面
+
+
+  lieshu++;
+}
+//添加文字主键
+function add(type,TXT,shu)
+{
+  var divs = "div" + lieshu;
+  var divt = document.createElement("div");//创建一个LI
+  divt.id = divs;
+  //divt.style.height = "20%";
+  divt.style.width = "90%";
+  divt.style.left = "5%";
+  divt.style.position = "relative";
+  //divt.style.transform = "skew(12deg)";
+  document.getElementById(shu).appendChild(divt);
+
+
+  var tiaoguo = "";
+  switch(type){
+    case "p":tiaoguo = 1; break;
+    case "a":tiaoguo = 1; break;
+    case "h1":tiaoguo = 2; break;
+    case "h2":tiaoguo = 2; break;
+  }
+
+  
+
+
+  var ele = document.createElement(type);//创建一个LI
+  ele.innerHTML = TXT.substr(tiaoguo,10000);//修改里面的属性s
+  //ele.style.width = "100%";
+  ele.className = type;
+  //ele.style.float = "left";
+  document.getElementById(divs).appendChild(ele);//把LI放到ID="zhu"的lu里面
+
+
+  lieshu++;
+}
+//添加主键
+function adddiv(id)
+{
+  var ele = document.createElement("div");//创建一个LI
+  ele.id = "xianshineirong" + id;
+  ele.style.height = "100%";
+  ele.style.width = "100%";
+  document.getElementById(id).appendChild(ele);//把LI放到ID="zhu"的lu里面
+}
+
+function duqutxtneirong(URss){
+  let file_url = "https://raw.githubusercontent.com/wuxingwushu/wuxingwushu.github.io/main/" + URss;
+  let xhr = new XMLHttpRequest();
+  xhr.open("get", file_url, true);
+  xhr.responseType = "blob";
+  xhr.onload = function () {
+    if (this.status == 200) {
+      console.log(this.response)
+      const reader = new FileReader()
+      reader.onload = function () {
+        
+        shanchu("xtxt"+ulid)
+        adddiv(ulid-2);
+        var idming = "xianshineirong" + (ulid-2);
+
+        for (let index = 0; index < reader.result.split("\n").length; index++) {
+          for(let lei = 0; lei < leixing.length;lei++){
+            if(reader.result.split("\n")[index].search(leixing[lei]) != -1){
+              add(leixing[lei],reader.result.split("\n")[index],idming);
+            }
+            if(reader.result.split("\n")[index].search("<代码>") != -1){
+              index++;
+              console.log("chachacha :  " + reader.result.split("\n")[index].search("</代码>"));
+              while(reader.result.split("\n")[index].search("</代码>") == -1){
+                addma(leixing[lei],reader.result.split("\n")[index],idming);
+                index++;
+              }
+            }
+          }
+        }
+
+
+      }
+      reader.readAsText(this.response);
+    }
+  };
+  xhr.send();
 }
